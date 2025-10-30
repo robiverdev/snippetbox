a999dc45317baf7c9efeeefdc477fb772f2ea431
Robi Verdev <robi.verdev1@gmail.com>
2025-10-30 13:15:33 +0100

    set up basic web server on port 4000 with home route

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
-func main(){
-    fmt.Println("Hello world!")
+// Define a home handler function which writes a byte slice
+func home(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Hello from Snippetbox"))
+
+	if err != nil {
+		log.Println("Error writing response:", err)
+		return
+	}
+}
+
+func main() {
+	mux := http.NewServeMux() // Initialize new mux server
+	mux.HandleFunc("/", home) // response body - / catch all
+
+	log.Print("Starting server on :4000")
+
+	err := http.ListenAndServe(":4000", mux) // passing in TCP network and mux server
+	log.Fatal(err)                           // terminates the program and logs the err msg
 }
