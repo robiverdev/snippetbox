84765d543bc920fe6587ac8a1b2e9a31476617b9
Robi Verdev <robiverdev@Robis-MacBook-Pro.local>
2025-11-01 19:25:52 +0100

    added method based routing GET/POST

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 // Define a home handler function which writes a byte slice
 func home(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("Hello from Snippetbox"))
+	_, err := w.Write([]byte("Hello from Snippet-box"))
 
 	if err != nil {
 		log.Println("Error writing response:", err)
@@ -25,7 +25,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	msg := fmt.Sprintf("Display a specific snippet with ID - %d", id)
-	_, err := w.Write([]byte(msg))
+	_, err = w.Write([]byte(msg))
 	if err != nil {
 		log.Printf("Error writing response: %v", err)
 		return
@@ -41,11 +41,20 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Save a new snippet"))
+
+	if err != nil {
+		fmt.Println("Error saving new snippet: ", err)
+	}
+}
+
 func main() {
-	mux := http.NewServeMux()                         // Initialize new mux server
-	mux.HandleFunc("/{$}", home)                      // response body - / catch all - /{$} match a single slash, followed by nothing else
-	mux.HandleFunc("/snippet/view/{id}", snippetView) // {id} wildcard
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux := http.NewServeMux()                             // Initialize new mux server
+	mux.HandleFunc("GET /{$}", home)                      // response body - / catch all - /{$} match a single slash, followed by nothing else
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView) // {id} wildcard
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
 	log.Print("Starting server on :4000")
 
