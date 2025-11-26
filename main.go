eebaee2eae0795b9ea23296aaca5ed8da9646760
robiverdev <robi.verdev1@gmail.com>
2025-11-26 19:28:48 +0100

    add simple http server with home route

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+// Define a home handler function which writes a byte slice
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	mux := http.NewServeMux() // Initialize a new servermux
+	mux.HandleFunc("/", home) // Register the home function as handler for the "/" URL pattern
+
+	log.Print("Starting server on: 4000") // Print a log message
+
+	err := http.ListenAndServe(":4000", mux) // Start a new web server (TCP network address to listen on, servermux)
+	log.Fatal(err)                           // Log err message and terminate the program
 }
