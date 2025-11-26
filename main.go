2e1330a7d589f74f3c4b942bece4cb310754f677
robiverdev <robi.verdev1@gmail.com>
2025-11-26 19:39:19 +0100

    add view and create routes

--- main.go
+++ main.go
@@ -10,9 +10,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a snippet"))
+}
+
 func main() {
-	mux := http.NewServeMux() // Initialize a new servermux
-	mux.HandleFunc("/", home) // Register the home function as handler for the "/" URL pattern
+	mux := http.NewServeMux()    // Initialize a new servermux
+	mux.HandleFunc("/{$}", home) // Register the home function as handler for the "/" URL pattern. {$} - match a single slash, followed by nothing else
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on: 4000") // Print a log message
 
